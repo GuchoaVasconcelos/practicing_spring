436684f587b597765a2b096a5539802f37e3b5ac
Gabriel Uchoa <gabriel.uchoa.vasconcelos@gmail.com>
2021-04-21 13:29:43 -0300

    feat: create user entity

---
+++ src/main/java/com/aprendendo/projetoteste/entities/User.java
@@ -0,0 +1,50 @@
+package com.aprendendo.projetoteste.entities;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Objects;
+
+
+@Entity
+@Data
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, String phone, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return getId().equals(user.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
