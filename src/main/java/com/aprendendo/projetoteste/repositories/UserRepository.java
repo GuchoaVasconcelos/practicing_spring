937173a3549856786751c67cd59c7caae5af5f73
Gabriel Uchoa <gabriel.uchoa.vasconcelos@gmail.com>
2021-04-21 13:53:56 -0300

    fix: add spring notation to repository

--- src/main/java/com/aprendendo/projetoteste/repositories/UserRepository.java
+++ src/main/java/com/aprendendo/projetoteste/repositories/UserRepository.java
@@ -2,6 +2,9 @@ package com.aprendendo.projetoteste.repositories;
 
 import com.aprendendo.projetoteste.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
+
 }
