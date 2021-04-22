80fb2ae95c7cc7e65d4a138ffa799f1606c634a5
Gabriel Uchoa <gabriel.uchoa.vasconcelos@gmail.com>
2021-04-22 07:34:09 -0300

    feat: create order controller

---
+++ src/main/java/com/aprendendo/projetoteste/controller/OrderController.java
@@ -0,0 +1,30 @@
+package com.aprendendo.projetoteste.controller;
+
+
+import com.aprendendo.projetoteste.entities.Order;
+import com.aprendendo.projetoteste.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/orders")
+public class OrderController {
+
+    @Autowired
+    private OrderService service;
+
+    @GetMapping
+    public ResponseEntity<List<Order>> findAll(){
+        List<Order> orders = service.findAll();
+        return ResponseEntity.ok().body(orders);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Order> findById(@PathVariable Long id){
+        Order order = service.findById(id);
+        return ResponseEntity.ok().body(order);
+    }
+}
