f9bb58821cb97ac4b210d9bf26fdd5fc777da4dc
Gabriel Uchoa <gabriel.uchoa.vasconcelos@gmail.com>
2021-04-22 07:33:54 -0300

    feat: create order entity

---
+++ src/main/java/com/aprendendo/projetoteste/entities/Order.java
@@ -0,0 +1,45 @@
+package com.aprendendo.projetoteste.entities;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Data
+@Table(name = "tb_order")
+public class Order{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+    private Instant moment;
+    @ManyToOne
+    private User client;
+
+    public Order() {
+    }
+
+    public Order(Long id, Instant moment, User client) {
+        this.id = id;
+        this.moment = moment;
+        this.client = client;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Order)) return false;
+        Order order = (Order) o;
+        return getId().equals(order.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
