ed717a14d2471c85c89afa730a97bcb1159b55cb
Gabriel Uchoa <gabriel.uchoa.vasconcelos@gmail.com>
2021-04-22 07:33:13 -0300

    feat: add function to find by id to user controller

--- src/main/java/com/aprendendo/projetoteste/controller/UserController.java
+++ src/main/java/com/aprendendo/projetoteste/controller/UserController.java
@@ -5,9 +5,7 @@ import com.aprendendo.projetoteste.entities.User;
 import com.aprendendo.projetoteste.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +20,11 @@ public class UserController {
     public ResponseEntity<List<User>> findAll(){
         List<User> users = service.findAll();
         return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User user = service.findById(id);
+        return ResponseEntity.ok().body(user);
     }
 }
