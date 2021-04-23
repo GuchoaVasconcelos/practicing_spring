3dbbee5dba3390ef9faeb1156a5160eb154b76b7
Gabriel Uchoa <gabriel.uchoa.vasconcelos@gmail.com>
2021-04-23 09:06:35 -0300

    feat: create entity OrderItemPK

---
+++ src/main/java/com/aprendendo/projetoteste/entities/pk/OrderItemPK.java
@@ -0,0 +1,40 @@
+package com.aprendendo.projetoteste.entities.pk;
+
+import com.aprendendo.projetoteste.entities.Order;
+import com.aprendendo.projetoteste.entities.Product;
+import lombok.Data;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+import java.util.Objects;
+
+
+@Embeddable
+@Data
+public class OrderItemPK implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof OrderItemPK)) return false;
+        OrderItemPK that = (OrderItemPK) o;
+        return getOrder().equals(that.getOrder()) && getProduct().equals(that.getProduct());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getOrder(), getProduct());
+    }
+}
