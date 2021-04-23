ae4b6d587d1679d6bdfabcea20ca7b7d612c25f8
Gabriel Uchoa <gabriel.uchoa.vasconcelos@gmail.com>
2021-04-23 09:05:06 -0300

    feat: add orderStatus and set of items to Order

--- src/main/java/com/aprendendo/projetoteste/entities/Order.java
+++ src/main/java/com/aprendendo/projetoteste/entities/Order.java
@@ -1,16 +1,19 @@
 package com.aprendendo.projetoteste.entities;
 
+import com.aprendendo.projetoteste.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import javax.persistence.*;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Data
 @Table(name = "tb_order")
-public class Order{
+public class Order {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,15 +21,21 @@ public class Order{
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
+    private Integer orderStatus;
     @ManyToOne
+    @JoinColumn(name = "client_id")
     private User client;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -36,6 +45,16 @@ public class Order{
         if (!(o instanceof Order)) return false;
         Order order = (Order) o;
         return getId().equals(order.getId());
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override
